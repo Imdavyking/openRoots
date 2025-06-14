8010803be2dc30bd3206bde13232650fe31bb56c
imdavyking <yungdynamic53@gmail.com>
2025-06-14 13:10:24 +0100

    chore: fixed connect button

--- frontend/src/components/ConnectWalletButton.tsx
+++ frontend/src/components/ConnectWalletButton.tsx
@@ -1,28 +1,27 @@
 import { ellipsify } from "../utils/ellipsify";
-import { useConnectModal } from "@tomo-inc/tomo-evm-kit";
+import { useAccountModal, useConnectModal } from "@tomo-inc/tomo-evm-kit";
+import { useAccount } from "wagmi";
 
 export default function ConnectWalletButton() {
   const { openConnectModal } = useConnectModal();
-  const account = {
-    address: null,
-  };
-  const isConnecting = false;
+  const { openAccountModal } = useAccountModal();
+  const { address, isConnected } = useAccount();
+
   return (
     <div className="flex flex-col items-center space-y-2">
       <button
-        onClick={openConnectModal}
+        onClick={isConnected ? openAccountModal : openConnectModal}
         className={`cursor-pointer px-6 py-2 ${
-          account?.address
+          address
             ? "bg-red-600 hover:bg-red-700"
             : "bg-blue-600 hover:bg-blue-700"
         } text-white font-semibold rounded-lg transition-all duration-300 disabled:opacity-50`}
-        disabled={false}
       >
-        {isConnecting
-          ? "Connecting..."
-          : account?.address
-          ? `Disconnect (${ellipsify(account.address)})`
-          : "Connect Wallet"}
+        {!isConnected
+          ? "Connect Wallet"
+          : address
+          ? `Disconnect (${ellipsify(address)})`
+          : "Connecting..."}
       </button>
     </div>
   );
